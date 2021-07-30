64f314a1a498714965859ae692ba4e16f6b3345b
AnuragAD <44764272+AnuragAD@users.noreply.github.com>
2021-07-30 17:41:34 +0530

    Add files via upload

---
+++ Day 1 (28.07.2021)/Pareadic_Case.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+   
+ 
+public static void Main(String []args)
+{
+    int age;
+    Console.WriteLine("Enter the Age of Patient");
+    age = Convert.ToInt32(Console.ReadLine());
+    if(age<16){
+     Console.WriteLine("Paediatric case");
+    }
+    else
+    Console.WriteLine("Adult case");
+}
+    
+}
