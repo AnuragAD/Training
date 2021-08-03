feb06f45b630f82f36bca2a939985a4652dd61a2
AnuragAD <44764272+AnuragAD@users.noreply.github.com>
2021-08-03 16:57:52 +0530

    Add files via upload

---
+++ Day 5 (03.08.2021)/1. Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList AL = new ArrayList();
+            AL.Add("TIRAMISU LAYER CAKE");
+            AL.Add("DEVIL'S FOOD CAKE");
+            AL.Add("LEMON SOUFFLES");
+            AL.Add("WHITE CHOCOLATE RASPBERRY CHEESECAKE");
+            AL.Add("BAKLAVA");
+            FileStream fs = new FileStream("E:/Training/Day 5/files/desert.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter sw = new StreamWriter(fs);
+            foreach (string s in AL)
+            {
+                sw.Write(s + "\n");
+            }
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+            FileStream fs1 = new FileStream("E:/Training/Day 5/files/desert.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sa = new StreamReader(fs1);
+            string line = sa.ReadLine();
+            while (line != null)
+            {
+                Console.WriteLine(line);
+                line = sa.ReadLine();
+            }
+            string fileName = @"E:/Training/Day 5/files/desert.txt";
+            FileInfo fi = new FileInfo(fileName);
+            bool exists = fi.Exists;
+            if (fi.Exists)
+            {
+                DateTime creationTime = fi.CreationTime;
+                long size = fi.Length;
+                Console.WriteLine("File Size in Bytes: {0}", size);
+                Console.WriteLine("Creation time: {0}", creationTime);
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Day 5 (03.08.2021)/2. Employee.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_5
+{
+    [Serializable]
+    class Employee
+    {
+        int Id;
+        String Name;
+        String Gender;
+        int age;
+        public static void Main()
+        {
+            Employee obj = new Employee();
+            obj.Id = 1;
+            obj.Name = "Anurag";
+            obj.Gender = "Male";
+            obj.age = 23;
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(@"E:/Training/Day 5/files/employee.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            formatter.Serialize(stream, obj);
+            stream.Close();
+
+            stream = new FileStream(@"E:/Training/Day 5/files/employee.txt", FileMode.Open, FileAccess.Read);
+            Employee objnew = (Employee)formatter.Deserialize(stream);
+            Console.WriteLine(objnew.Id);
+            Console.WriteLine(objnew.Name);
+            Console.WriteLine(objnew.Gender);
+            Console.WriteLine(objnew.age);
+            Console.ReadKey();
+           
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Console.WriteLine("Enter Employee Id: ");
+//int id = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Enter Employee Name: ");
+//string name = Console.ReadLine();
+//Console.WriteLine("Enter Employee Gender: ");
+//string gender = Console.ReadLine();
+//Console.WriteLine("Enter Employee Age: ");
+//int age = Convert.ToInt32(Console.ReadLine());
+//ArrayList AL = new ArrayList();
+//AL.Add(id);
+//AL.Add(name);
+//AL.Add(gender);
+//AL.Add(age);
+//Employee sobj = new Employee();
