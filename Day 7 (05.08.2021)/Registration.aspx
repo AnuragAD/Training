c8a7db3a55d71e6a7744420cf7f3b4bb7ad43738
AnuragAD <44764272+AnuragAD@users.noreply.github.com>
2021-08-05 15:49:33 +0530

    Add files via upload

---
+++ Day 7 (05.08.2021)/Registration.aspx
@@ -0,0 +1,80 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Registration.aspx.cs" Inherits="Day7.Registration" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+
+
+    <title>User Registration </title>
+</head>
+<body>
+        <div class="container">
+             <center> <h1> Registration Form</h1> </center> 
+        <div class="card">
+            <div class="card-body">
+          <form id="form1" runat="server">
+                      <div class="form-control">
+                      <div class="form-group">
+                         <asp:Label ID="Label1" runat="server" Text="Label">First Name</asp:Label> 
+                          <asp:TextBox ID="TextBox1" runat="server" required="true"></asp:TextBox>
+                      </div> <br />
+                      <div class="form-group">
+                         <asp:Label ID="Label2" runat="server" Text="Label" required="true">Last Name</asp:Label> 
+                          <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                      </div>
+                          <br />
+                      <div class="form-group">
+                         <asp:Label ID="Label3" runat="server" Text="Label">Gender</asp:Label> 
+                          <asp:RadioButtonList ID="RadioButtonList1" runat="server">
+                              <asp:ListItem Value="1">Male</asp:ListItem>
+                              <asp:ListItem Value="1">Female</asp:ListItem>
+                          </asp:RadioButtonList>
+                      </div><br />
+                    <div class="form-group">
+                         <asp:Label ID="Label4" runat="server" Text="Label" required="true">Age</asp:Label> 
+                          <asp:TextBox ID="TextBox3" runat="server" TextMode="Number"></asp:TextBox>
+                      </div><br />
+                    <div class="form-group">
+                         <asp:Label ID="Label5" runat="server" Text="Label" required="true">State</asp:Label> 
+                        <asp:DropDownList ID="DropDownList1" runat="server">
+                                <asp:ListItem>Andaman and Nicobar Islands</asp:ListItem>
+                                <asp:ListItem>Andhra Pradesh</asp:ListItem>
+                                <asp:ListItem>Arunachal Pradesh</asp:ListItem>
+                                <asp:ListItem>Himachal Pradesh</asp:ListItem>
+                                <asp:ListItem>Madhya Pradesh</asp:ListItem>
+                                <asp:ListItem>Maharashtra</asp:ListItem>
+                                <asp:ListItem>Manipur</asp:ListItem>
+                                <asp:ListItem>Meghalaya</asp:ListItem>
+                                <asp:ListItem>Mizoram</asp:ListItem>
+                                <asp:ListItem>Nagaland</asp:ListItem>
+                                <asp:ListItem>Orissa</asp:ListItem>
+                                <asp:ListItem>Punjab</asp:ListItem>
+                                <asp:ListItem>Rajasthan</asp:ListItem>
+                                <asp:ListItem>Sikkim</asp:ListItem>
+                        </asp:DropDownList>
+                      </div><br />
+                      <div class="form-group">
+                          <asp:Button ID="Button1" runat="server" Text="Populate cities" OnClick="Button1_Click" />
+                      </div><br />
+                          <div class="form-group">
+                              <asp:ListBox ID="ListBox1" runat="server" Width="400px"></asp:ListBox>
+                      </div><br />
+                          <div class="form-group">
+                          <asp:Button ID="Button2" runat="server" Text="Submit" OnClick="Button2_Click" />
+                      </div><br />
+                          <div class="form-group">
+                             <asp:ListBox ID="ListBox2" runat="server" Width="400px"></asp:ListBox>
+                      </div>
+                   </div>
+    </form>
+       </div>  
+      </div>
+   
+    </div>
+</body>
+</html>
---
+++ Day 7 (05.08.2021)/Registration.aspx.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Day7
+{
+    public partial class Registration : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if(DropDownList1.SelectedValue == "Andaman and Nicobar Islands")
+            {
+                ListBox1.Items.Add("Port Blair");
+                ListBox1.Items.Add("Arong");
+                ListBox1.Items.Add("Manglutan");
+                ListBox1.Items.Add("BombooFlat");
+                ListBox1.Items.Add("Mayabunder");
+                ListBox1.Items.Add("Digilpur");
+                ListBox1.Items.Add("Perka");
+            }
+            else if (DropDownList1.SelectedValue == "Andhra Pradesh")
+            {
+                ListBox1.Items.Clear();
+                ListBox1.Items.Add("Anantapur");
+                ListBox1.Items.Add("Chittoor");
+                ListBox1.Items.Add("Guntur");
+                ListBox1.Items.Add("Kurnool");
+                ListBox1.Items.Add("Srikakulam");
+                ListBox1.Items.Add("Visakhapatnam");
+                ListBox1.Items.Add("West Godavari");
+            }
+            else if (DropDownList1.SelectedValue == "Arunachal Pradesh")
+            {
+                ListBox1.Items.Clear();
+                ListBox1.Items.Add("City 1");
+                ListBox1.Items.Add("City 2");
+                ListBox1.Items.Add("City 3");
+                ListBox1.Items.Add("City 4");
+                ListBox1.Items.Add("City 5");
+                ListBox1.Items.Add("City 6");
+                ListBox1.Items.Add("City 7");
+            }
+            else if (DropDownList1.SelectedValue == "Himachal Pradesh")
+            {
+                ListBox1.Items.Clear();
+                ListBox1.Items.Add("City 1");
+                ListBox1.Items.Add("City 2");
+                ListBox1.Items.Add("City 3");
+                ListBox1.Items.Add("City 4");
+                ListBox1.Items.Add("City 5");
+                ListBox1.Items.Add("City 6");
+                ListBox1.Items.Add("City 7");
+            }
+            else if (DropDownList1.SelectedValue == "Madhya Pradesh")
+            {
+                ListBox1.Items.Clear();
+                ListBox1.Items.Add("Satna");
+                ListBox1.Items.Add("City 2");
+                ListBox1.Items.Add("City 3");
+                ListBox1.Items.Add("City 4");
+                ListBox1.Items.Add("City 5");
+                ListBox1.Items.Add("City 6");
+                ListBox1.Items.Add("City 7");
+            }
+            else if (DropDownList1.SelectedValue == "Maharashtra")
+            {
+                ListBox1.Items.Clear();
+                ListBox1.Items.Add("Mumbai");
+                ListBox1.Items.Add("City 2");
+                ListBox1.Items.Add("City 3");
+                ListBox1.Items.Add("City 4");
+                ListBox1.Items.Add("City 5");
+                ListBox1.Items.Add("City 6");
+                ListBox1.Items.Add("City 7");
+            }
+            else if (DropDownList1.SelectedValue == "Manipur")
+            {
+                ListBox1.Items.Clear();
+                ListBox1.Items.Add("Imphal");
+                ListBox1.Items.Add("City 2");
+                ListBox1.Items.Add("City 3");
+                ListBox1.Items.Add("City 4");
+                ListBox1.Items.Add("City 5");
+                ListBox1.Items.Add("City 6");
+                ListBox1.Items.Add("City 7");
+            }
+            else if (DropDownList1.SelectedValue == "Meghalaya")
+            {
+                ListBox1.Items.Clear();
+                ListBox1.Items.Add("Nongstoin");
+                ListBox1.Items.Add("City 2");
+                ListBox1.Items.Add("City 3");
+                ListBox1.Items.Add("City 4");
+                ListBox1.Items.Add("City 5");
+                ListBox1.Items.Add("City 6");
+                ListBox1.Items.Add("City 7");
+            }
+            else if (DropDownList1.SelectedValue == "Mizoram")
+            {
+                ListBox1.Items.Clear();
+                ListBox1.Items.Add("Aziwal");
+                ListBox1.Items.Add("City 2");
+                ListBox1.Items.Add("City 3");
+                ListBox1.Items.Add("City 4");
+                ListBox1.Items.Add("City 5");
+                ListBox1.Items.Add("City 6");
+                ListBox1.Items.Add("City 7");
+            }
+            else if (DropDownList1.SelectedValue == "Nagaland")
+            {
+                ListBox1.Items.Clear();
+                ListBox1.Items.Add("Kohima");
+                ListBox1.Items.Add("City 2");
+                ListBox1.Items.Add("City 3");
+                ListBox1.Items.Add("City 4");
+                ListBox1.Items.Add("City 5");
+                ListBox1.Items.Add("City 6");
+                ListBox1.Items.Add("City 7");
+            }
+            else if (DropDownList1.SelectedValue == "Orissa")
+            {
+                ListBox1.Items.Clear();
+                ListBox1.Items.Add("Bhubneshwar");
+                ListBox1.Items.Add("City 2");
+                ListBox1.Items.Add("City 3");
+                ListBox1.Items.Add("City 4");
+                ListBox1.Items.Add("City 5");
+                ListBox1.Items.Add("City 6");
+                ListBox1.Items.Add("City 7");
+            }
+            else if (DropDownList1.SelectedValue == "Punjab")
+            {
+                ListBox1.Items.Clear();
+                ListBox1.Items.Add("Chandigarh");
+                ListBox1.Items.Add("City 2");
+                ListBox1.Items.Add("City 3");
+                ListBox1.Items.Add("City 4");
+                ListBox1.Items.Add("City 5");
+                ListBox1.Items.Add("City 6");
+                ListBox1.Items.Add("City 7");
+            }
+            else if (DropDownList1.SelectedValue == "Rajasthan")
+            {
+                ListBox1.Items.Clear();
+                ListBox1.Items.Add("Jaipur");
+                ListBox1.Items.Add("City 2");
+                ListBox1.Items.Add("City 3");
+                ListBox1.Items.Add("City 4");
+                ListBox1.Items.Add("City 5");
+                ListBox1.Items.Add("City 6");
+                ListBox1.Items.Add("City 7");
+            }
+            else if (DropDownList1.SelectedValue == "Sikkim")
+            {
+                ListBox1.Items.Clear();
+                ListBox1.Items.Add("Gangtok");
+                ListBox1.Items.Add("City 2");
+                ListBox1.Items.Add("City 3");
+                ListBox1.Items.Add("City 4");
+                ListBox1.Items.Add("City 5");
+                ListBox1.Items.Add("City 6");
+                ListBox1.Items.Add("City 7");
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            ListBox2.Items.Clear();
+            ListBox2.Items.Add(TextBox1.Text);
+            ListBox2.Items.Add(TextBox2.Text);
+            ListBox2.Items.Add(RadioButtonList1.SelectedItem.Value);
+            ListBox2.Items.Add(TextBox3.Text);
+            ListBox2.Items.Add(DropDownList1.SelectedValue);
+            ListBox2.Items.Add(ListBox1.SelectedValue);
+        }
+    }
+}
